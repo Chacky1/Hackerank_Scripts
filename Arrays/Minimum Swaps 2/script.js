c52175c67141ab22a6703e829b8391a960d90581
Chacky1 <th.alban@outlook.fr>
2021-03-18 14:33:01 +0100

    Minimum Swaps 2 passed

---
+++ Arrays/Minimum Swaps 2/script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the minimumSwaps function below.
+function minimumSwaps(arr) {
+    console.log(arr)
+    let numberOfSwaps = 0
+    for (let i = 0; i < arr.length; i++) {
+        while (arr[i] != i+1) {
+            const tmp = arr[i]
+            arr[i] = arr[tmp-1]
+            arr[tmp-1] = tmp
+            numberOfSwaps++
+        }
+    }
+    return numberOfSwaps
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const res = minimumSwaps(arr);
+
+    ws.write(res + '\n');
+
+    ws.end();
+}
+
